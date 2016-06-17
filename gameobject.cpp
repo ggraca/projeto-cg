5607899ce4da101982781e59b6779c9028b596e6
GGraca <ggracase@gmail.com>
2016-06-17 17:39:14 +0100

    Updated Gameobject class

--- gameobject.cpp
+++ gameobject.cpp
@@ -2,19 +2,32 @@
 
 GameObject::GameObject(Vector3* a){
   this->pos = a;
+  this->rot = new Vector3(0, 0, 0);
+  this->mov = new Vector3(0, 0, 0);
+  this->ang_mov = new Vector3(0, 0, 0);
 }
 
-void GameObject::draw(){
-  GLfloat bule=0.5;
-
-  glColor4f(1.0, 0.0, 0.0, 0.5);//Definir previamente o LARANJA (#define LARANJA ...)
-  glPushMatrix();
-    glTranslatef(this->pos->x, this->pos->y, this->pos->z);
-    //glRotatef(90, 1, 0, 0);
-    glutSolidTeapot (bule);
-  glPopMatrix();
+void GameObject::add_child(GameObject* go){
+  this->children.push_back(go);
 }
 
 void GameObject::update(){
+  glPushMatrix();
+    glRotatef(rot->x, 1, 0, 0);
+    glRotatef(rot->y, 0, 1, 0);
+    glRotatef(rot->z, 0, 0, 1);
 
+    glTranslatef(this->pos->x, this->pos->y, this->pos->z);
+
+    for(int i = 0; i < (int)children.size(); i++)
+      children[i]->update();
+
+    this->draw();
+  glPopMatrix();
+}
+
+void GameObject::draw(){
+  glColor4f(1.0, 0.0, 0.0, 0.5);
+  glutSolidTorus (0.5, 1, 20, 20);
+  //glutSolidTeapot (bule);
 }
--- gameobject.h
+++ gameobject.h
@@ -4,8 +4,10 @@ class GameObject{
     Vector3* rot;
     Vector3* mov;
     Vector3* ang_mov;
+    vector<GameObject*> children;
 
     GameObject(Vector3* a);
     void update();
     void draw();
+    void add_child(GameObject* go);
 };
